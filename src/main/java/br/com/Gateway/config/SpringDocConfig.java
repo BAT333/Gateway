6d05c227a715967ca61ce821362b3e4e008c9460
RafaelBat-alves <152424427+RafaelBat-alves@users.noreply.github.com>
2024-09-29 15:55:05 -0300

    logging the application and refactoring the code by removing functions that are no longer

---
+++ src/main/java/br/com/Gateway/config/SpringDocConfig.java
@@ -0,0 +1,25 @@
+package br.com.Gateway.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("Cclientelient service api")
+                                .description("THIS PART OF THE API IS RESPONSIBLE FOR CUSTOMER REGISTRATION AND OTHER DELETE, UPDATE, SIMPLE SEARCH FUNCTIONALITIES")
+                                .version("v0.0.1")
+                )
+                .components(new Components()
+                        .addSecuritySchemes("bearer-key",
+                                new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")));
+    }
+}
--- src/main/java/br/com/Gateway/filter/AuthenticationFilter.java
+++ src/main/java/br/com/Gateway/filter/AuthenticationFilter.java
@@ -1,5 +1,6 @@
 package br.com.Gateway.filter;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
@@ -9,7 +10,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+import java.time.LocalDateTime;
+
+
 @Component
+@Slf4j
 public class AuthenticationFilter extends AbstractGatewayFilterFactory<AuthenticationFilter.Config> {
 
     @Autowired
@@ -19,6 +24,10 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     @Lazy
     private AuthService authService;
 
+    private final LocalDateTime dataTime = LocalDateTime.now();
+
+
+
     public AuthenticationFilter() {
         super(Config.class);
     }
@@ -27,8 +36,15 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
     public GatewayFilter apply(AuthenticationFilter.Config config) {
         return (exchange, chain) -> {
             if (valid.isSecured.test(exchange.getRequest())) {
+
+                var originHeader = exchange.getRequest().getURI();
+
+                log.info(originHeader+" "+ dataTime);
+
+
                 // Checks if the Authorization header is present
                 if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
+                    log.error("Missing authorization header: "+ dataTime);
                     return Mono.error(new RuntimeException("Missing authorization header"));
                 }
 
@@ -37,6 +53,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                 if (authHeader != null && authHeader.startsWith("Bearer ")) {
                     authHeader = authHeader.substring(7).replace("Bearer ", "");
                 } else {
+                    log.error("Invalid authorization header format: "+ dataTime);
                     return Mono.error(new RuntimeException("Invalid authorization header format"));
                 }
 
@@ -46,6 +63,7 @@ public class AuthenticationFilter extends AbstractGatewayFilterFactory<Authentic
                         .then(chain.filter(exchange))  // Continue reactive flow if authentication is successful
                         .onErrorResume(e -> {
                             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                            log.error("UNAUTHORIZED USER: "+ dataTime);
                             return exchange.getResponse().setComplete();
                         });
             }
